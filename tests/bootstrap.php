ce60a798fa25daaa0bee68afcaaf53d34838484a
ezrabynx <ezrabynx@gmail.com>
2014-06-10 20:36:12 -0400

    Travis Composer Updates

---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Simple Laravel QrCode Generator
+ * A simple wrapper for the popular BaconQrCode made for Laravel.
+ *
+ * @link http://www.simplesoftware.io
+ * @author SimpleSoftware support@simplesoftware.io
+ *
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
