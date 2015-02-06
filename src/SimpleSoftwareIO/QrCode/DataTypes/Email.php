1018b85fc798a1ddd9017c9e0bab91fff3cc8df3
Corey McCormick <corey@simplesoftware.io>
2015-02-05 19:40:40 -0500

    Add email helper.

---
+++ src/SimpleSoftwareIO/QrCode/DataTypes/Email.php
@@ -0,0 +1,117 @@
+<?php namespace SimpleSoftwareIO\QrCode\DataTypes;
+use BaconQrCode\Exception\InvalidArgumentException;
+
+/**
+ * Simple Laravel QrCode Generator
+ * A simple wrapper for the popular BaconQrCode made for Laravel.
+ *
+ * @link http://www.simplesoftware.io
+ * @author SimpleSoftware support@simplesoftware.io
+ *
+ */
+
+class Email implements DataTypeInterface {
+
+    /**
+     * The email address
+     *
+     * @var string
+     */
+    protected $email;
+
+    /**
+     * The subject of the email
+     *
+     * @var string
+     */
+    protected $subject;
+
+    /**
+     * The body of an email.
+     *
+     * @var string
+     */
+    protected $body;
+
+    /**
+     * Generates the DataType Object and sets all of its properties.
+     *
+     * @param $arguments
+     * @return void
+     */
+    public function create(Array $arguments)
+    {
+        $this->setProperties($arguments);
+    }
+
+    /**
+     * Returns the correct QrCode format.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->buildEmailString();
+    }
+
+    /*
+     * Builds the email string.
+     *
+     * @return string
+     */
+    private function buildEmailString()
+    {
+        $email = 'mailto:' . $this->email;
+
+        if (isset($this->subject) || isset($this->body))
+        {
+            $email .= '?';
+            $data = [
+                'subject' => $this->subject,
+                'body' => $this->body
+            ];
+            $email .= http_build_query($data);
+        }
+
+        return $email;
+    }
+
+    /**
+     * Sets the objects properties
+     *
+     * @param $arguments
+     */
+    private function setProperties($arguments)
+    {
+        //If passed as a single variable and not in an array
+        if (isset($arguments[0])) $this->setEmail($arguments[0]);
+        if (isset($arguments[1])) $this->subject = $arguments[1];
+        if (isset($arguments[2])) $this->body = $arguments[2];
+    }
+
+    /**
+     * Sets the email property
+     *
+     * @param $email
+     */
+    private function setEmail($email)
+    {
+        if ( $this->isValidEmail($email)) $this->email = $email;
+    }
+
+    /**
+     * Ensures an email is valid
+     *
+     * @param string $email
+     * @return bool
+     */
+    private function isValidEmail($email)
+    {
+        if ( ! filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new InvalidArgumentException('Invalid email provided');
+        }
+
+        return true;
+    }
+
+}
---
+++ tests/DataTypes/EmailTest.php
@@ -0,0 +1,64 @@
+<?php
+
+use SimpleSoftwareIO\QrCode\DataTypes\Email;
+
+/**
+ * Simple Laravel QrCode Generator
+ * A simple wrapper for the popular BaconQrCode made for Laravel.
+ *
+ * @link http://www.simplesoftware.io
+ * @author SimpleSoftware support@simplesoftware.io
+ *
+ */
+
+class EmailTest extends \PHPUnit_Framework_TestCase{
+
+    public function setUp()
+    {
+        $this->email = new Email();
+    }
+
+    public function test_it_generates_the_proper_format_when_only_an_email_address_is_supplied()
+    {
+        $this->email->create(['foo@bar.com']);
+
+        $properFormat = 'mailto:foo@bar.com';
+
+        $this->assertEquals($properFormat, strval($this->email));
+    }
+
+    public function test_it_generates_the_proper_format_when_an_email_subject_and_body_are_supplied()
+    {
+        $this->email->create(['foo@bar.com', 'foo', 'bar']);
+
+        $properFormat = 'mailto:foo@bar.com?subject=foo&body=bar';
+
+        $this->assertEquals($properFormat, strval($this->email));
+    }
+
+    public function test_it_generates_the_proper_format_when_an_email_and_subject_are_supplied()
+    {
+        $this->email->create(['foo@bar.com', 'foo']);
+
+        $properFormat = 'mailto:foo@bar.com?subject=foo';
+
+        $this->assertEquals($properFormat, strval($this->email));
+    }
+
+    public function test_it_generates_the_proper_format_when_only_a_subject_is_provided()
+    {
+        $this->email->create([null, 'foo']);
+
+        $properFormat = 'mailto:?subject=foo';
+
+        $this->assertEquals($properFormat, strval($this->email));
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function test_it_throws_an_exception_when_an_invalid_email_is_given()
+    {
+        $this->email->create(['foo']);
+    }
+}
