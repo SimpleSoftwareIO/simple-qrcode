609080bd5e4435f9329d2ccc3ff6951518b28773
Thomas Krug <thomas@simplesoftware.io>
2017-05-17 01:06:19 +0000

    Apply fixes from StyleCI

--- src/SimpleSoftwareIO/QrCode/BaconQrCodeGenerator.php
+++ src/SimpleSoftwareIO/QrCode/BaconQrCodeGenerator.php
@@ -3,14 +3,14 @@
 namespace SimpleSoftwareIO\QrCode;
 
 use BaconQrCode;
-use BaconQrCode\Writer;
+use BaconQrCode\Common\ErrorCorrectionLevel;
 use BaconQrCode\Encoder\Encoder;
 use BaconQrCode\Renderer\Color\Rgb;
 use BaconQrCode\Renderer\Image\Eps;
 use BaconQrCode\Renderer\Image\Png;
-use BaconQrCode\Renderer\Image\Svg;
-use BaconQrCode\Common\ErrorCorrectionLevel;
 use BaconQrCode\Renderer\Image\RendererInterface;
+use BaconQrCode\Renderer\Image\Svg;
+use BaconQrCode\Writer;
 
 class BaconQrCodeGenerator implements QrCodeInterface
 {
@@ -52,7 +52,7 @@ class BaconQrCodeGenerator implements QrCodeInterface
     /**
      * BaconQrCodeGenerator constructor.
      *
-     * @param Writer|null $writer
+     * @param Writer|null            $writer
      * @param RendererInterface|null $format
      */
     public function __construct(Writer $writer = null, RendererInterface $format = null)
@@ -96,7 +96,7 @@ class BaconQrCodeGenerator implements QrCodeInterface
      */
     public function merge($filepath, $percentage = .2, $absolute = false)
     {
-        if (function_exists('base_path') && ! $absolute) {
+        if (function_exists('base_path') && !$absolute) {
             $filepath = base_path().$filepath;
         }
 
@@ -265,7 +265,7 @@ class BaconQrCodeGenerator implements QrCodeInterface
     {
         $class = $this->formatClass($method);
 
-        if (! class_exists($class)) {
+        if (!class_exists($class)) {
             throw new \BadMethodCallException();
         }
 
