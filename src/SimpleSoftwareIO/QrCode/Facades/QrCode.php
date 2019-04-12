4b5c2cb5d0b7fb71b5e0498ab2252dd871b11878
Max Sky <maxsky@users.noreply.github.com>
2019-04-12 10:19:31 +0800

    Update QrCode.php, compatible with the IDE
    Make the PhpStorem/IDEA can be find method like generate and set size, color, etc.

--- src/SimpleSoftwareIO/QrCode/Facades/QrCode.php
+++ src/SimpleSoftwareIO/QrCode/Facades/QrCode.php
@@ -3,7 +3,22 @@
 namespace SimpleSoftwareIO\QrCode\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use SimpleSoftwareIO\QrCode\BaconQrCodeGenerator;
 
+/**
+ * @method static string|void generate($text, $filename = null)
+ * @method static BaconQrCodeGenerator merge($filepath, $percentage = .2, $absolute = false)
+ * @method static BaconQrCodeGenerator mergeString($content, $percentage = .2)
+ * @method static BaconQrCodeGenerator format($format)
+ * @method static BaconQrCodeGenerator size($pixels)
+ * @method static BaconQrCodeGenerator color($red, $green, $blue)
+ * @method static BaconQrCodeGenerator backgroundColor($red, $green, $blue)
+ * @method static BaconQrCodeGenerator errorCorrection($level)
+ * @method static BaconQrCodeGenerator margin($margin)
+ * @method static BaconQrCodeGenerator encoding($encoding)
+ *
+ * @see BaconQrCodeGenerator
+ */
 class QrCode extends Facade
 {
     /**
